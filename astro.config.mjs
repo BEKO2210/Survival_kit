fb30c8830e2c5e4543f1f417ea6fdf7379c14b19
Belkis Aslani <belkis.aslani@gmail.com>
2026-03-06 18:08:12 +0800

    Fix: base URL auf Survival_kit korrigiert

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://BEKO2210.github.io',
-  base: '/WW3',
+  base: '/Survival_kit',
 });
